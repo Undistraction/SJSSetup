6d4c28e0dcf649c8f1a2c0eb38df559de1b6e2e3
Pedr Browne <pedr.browne@gmail.com>
2015-12-08 09:57:59 +0000

    docs: Add comments to webpack config

--- config/webpack/dev.config.js
+++ config/webpack/dev.config.js
@@ -1,60 +1,54 @@
 var path = require('path');
 var webpack = require('webpack');
 var HtmlwebpackPlugin = require('html-webpack-plugin');
+// Pollyfill ES6 Promises
 require('es6-promise').polyfill();
 
+// Root directory of app
 const rootDir = path.join(__dirname, '../', '../');
 
+// Important paths within the app directory
 const PATHS = {
   app: path.join(rootDir, 'app'),
-  build: path.join(rootDir, 'build')
+  build: path.join(rootDir, 'build'),
+  node: path.join(rootDir, 'node_modules')
 };
-
-var node_dir = path.join(rootDir, 'node_modules');
 
 var config = {
 
+  // Add aliased vendors
   addVendor: function (name, path) {
     this.resolve.alias[name] = path;
     this.module.noParse.push(new RegExp(path));
   },
 
+  // Configure resolution of assets
   resolve: {
+    // Vendored aliases are added below using `config.addVendor`
     alias: {},
+    // Handle these extensions
     extensions: ['', '.js', '.jsx']
   },
 
   entry: PATHS.app,
-  
+
   output: {
     path: PATHS.build,
     filename: 'bundle.js'
   },
 
+  // Development Config
   devtool: 'inline-source-map',
 
-  module: {
-    noParse: [],
-    loaders: [
-      {
-        test: /\.scss$/,
-        loaders: ["style", "css?sourceMap", "autoprefixer-loader?browsers=last 4 version", "sass?sourceMap"],
-        include: PATHS.app
-      },
-      {
-        test: /\.jsx?$/,
-        loaders: ['babel', 'eslint'],
-        include: PATHS.app
-      }
-    ]
-  },
-
+  // Sass Loader Config
   sassLoader: {
+
     includePaths: [
-      path.join(node_dir, 'normalize-scss', 'sass'),
-      path.join(node_dir, 'normalize-scss', 'node_modules', 'support-for', 'sass')]
+      path.join(PATHS.node, 'normalize-scss', 'sass'),
+      path.join(PATHS.node, 'normalize-scss', 'node_modules', 'support-for', 'sass')]
   },
 
+  // Development Server Config
   devServer: {
     historyApiFallback: true,
     hot: true,
@@ -67,14 +61,42 @@ var config = {
     port: process.env.PORT
   },
 
+  // Configure Modules
+  module: {
+    noParse: [],
+    loaders: [
+      // JavaScript
+      {
+        test: /\.jsx?$/,
+        loaders: ['babel', 'eslint'],
+        include: PATHS.app
+      },
+      // Styles
+      {
+        test: /\.scss$/,
+        loaders: ["style", "css?sourceMap", "autoprefixer-loader?browsers=last 4 version", "sass?sourceMap"],
+        include: PATHS.app
+      },
+      // Images
+      {
+        test: /\.(png|jpg)$/,
+        loaders: ['url-loader?limit=8192']
+      }
+    ]
+  },
+
+  // Configure Plugins
   plugins: [
+    // Replacement of code on the fly without a full page reload
     new webpack.HotModuleReplacementPlugin(),
+    // Generate HTML index page automatically
     new HtmlwebpackPlugin({
       title: 'Kanban app'
     })
   ]
 };
 
-config.addVendor('normalize-scss', path.join(node_dir, 'normalize-scss', 'sass'));
+// Add vendored libs
+config.addVendor('normalize-scss', path.join(PATHS.node, 'normalize-scss', 'sass'));
 
 module.exports = config;
